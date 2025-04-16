0c5cbda9e9fe49dd2c7f4a097da443ca23ad3d5f
Praveen Kumar  Thakur <pravee1701@gmail.com>
2025-04-16 11:46:28 +0530

    auth appwrite

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,24 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from 'eslint-plugin-react'
+import importPlugin from 'eslint-plugin-import'
+import a11yPlugin from 'eslint-plugin-jsx-a11y'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettier from 'eslint-config-prettier'
 
 export default [
-  { ignores: ['dist'] },
+  { 
+    ignores: ['dist', 'build', 'node_modules', 'coverage', '*.min.js'] 
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -17,17 +27,68 @@ export default [
       },
     },
     plugins: {
+      'react': reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin,
+      'jsx-a11y': a11yPlugin,
+      'prettier': prettierPlugin
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      
+      // Error Prevention
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+      'no-duplicate-imports': 'error',
+      'no-undef': 'error',
+      'no-var': 'error',
+      'prefer-const': 'warn',
+      
+      // React specific rules
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-no-undef': 'error',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/self-closing-comp': 'warn',
+      
+      // Import organization
+      'import/first': 'warn',
+      'import/no-duplicates': 'error',
+      'import/order': ['warn', {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        'alphabetize': { order: 'asc', caseInsensitive: true }
+      }],
+      
+      // Accessibility basics
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/anchor-has-content': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      
+      // Formatting (Prettier will handle most of these)
+      'prettier/prettier': ['warn', {
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        arrowParens: 'always'
+      }]
     },
   },
+  prettier // Should be last to disable rules that conflict with Prettier
 ]
